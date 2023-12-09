547e63d1d20a4ee61c8daed7445d2c8a95e742f8
suchitra <suchitra9049@gmail.com>
2023-12-10 02:30:08 +0530

    Add. create multiple pair repo:

--- src/app/git-hub-action/git-hub-action.component.ts
+++ src/app/git-hub-action/git-hub-action.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import axios from "axios";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-git-hub-action',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './git-hub-action.component.html',
   styleUrl: './git-hub-action.component.scss'
@@ -15,14 +17,24 @@ export class GitHubActionComponent {
   accessToken: string = '';
   orgName: string = '';
   responseData: any;
-  repoName: string = 'pair-'
-  pairCount: number = 0
+  repoName: string = ''
+  currentAction: string = ''
+
+  repoPrefix: string = ""
+  repoCount: number = 0
 
   constructor() {
   }
 
+  setCurrentAction(action: string, event: Event) {
+    event.preventDefault();
+    this.currentAction = action;
+  }
+
   async createPair() {
     try {
+      console.log("Creating Single Repository", this.repoName)
+      console.log("****** Repo Name *****", this.repoName)
       const response = await this.createRepo(this.repoName);
       this.responseData = response.data; // Store response data
       console.log(this.responseData); // Display response in console (modify as needed)
@@ -42,4 +54,16 @@ export class GitHubActionComponent {
     });
     return response;
   }
+
+  async createMultiplePairRepo() {
+    console.log("Creating Multiple Pair Repository")
+    for (let i = 1; i <= this.repoCount; i++) {
+      const pairNumber = i.toString().padStart(2, '0'); // Ensure pair number has leading zero if needed
+      const repoName = `${this.repoPrefix}-${pairNumber}`;
+      console.log("****** Repo Name *****", repoName)
+      await this.createRepo(repoName);
+    }
+
+  }
+
 }
